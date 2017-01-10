dc79779823f4747e6843acf6ac7107e050a040a9
Alexander Moerman <amoerman@csc.com>
2017-01-10 14:33:49 +0100

    Implement factory completely, labyrinths can now be created randomly, with random tile orientations, treasures distributed randomly. Also add instance class for Show Board to display the board on the screen

--- Labyrinth/Factory.hs
+++ Labyrinth/Factory.hs
@@ -1,6 +1,5 @@
 module Labyrinth.Game (
-  labyrinthSize,
-  startingLabyrinth
+  labyrinthSize
 )
 where
 
@@ -12,17 +11,11 @@ where
 import Labyrinth.Models
 import Labyrinth.Helpers
 
-import qualified System.Random
 import Control.Monad.State
-
-labyrinthSize :: Int
-labyrinthSize = 7
-
-{-|
-  A completely empty labyrinth, containing no tiles
--}
-emptyLabyrinth :: UnfinishedBoard
-emptyLabyrinth = replicate (labyrinthSize * labyrinthSize) Nothing
+import Control.Arrow (second)
+import Data.List (sortBy)
+import Data.Ord (comparing)
+import System.Random
 
 fixedTiles :: [(Tile, Position)]
 fixedTiles = [
@@ -42,49 +35,119 @@ fixedTiles = [
     (Tile TShape Nothing West, (4,6)),
 
     (Tile Corner Nothing North,(6,0)),
-    (Tile TShape Nothing South,(6,2)),
-    (Tile TShape Nothing South,(6,4)),
+    (Tile TShape Nothing North,(6,2)),
+    (Tile TShape Nothing North,(6,4)),
     (Tile Corner Nothing West, (6,6))
   ]
 
+colorToStartingPosition :: Color -> Position
+colorToStartingPosition Yellow = (0,0)
+colorToStartingPosition Red    = (0,6)
+colorToStartingPosition Green  = (6,0)
+colorToStartingPosition Blue   = (6,6)
+
+allColors :: [Color]
+allColors = [(minBound :: Color ) .. ]
+
+allTreasures :: [Int]
+allTreasures = [0 .. 23]
+
+allStartingPositions :: [Position]
+allStartingPositions = map colorToStartingPosition allColors
+
+isStartingPosition :: Position -> Bool
+isStartingPosition pos = pos `elem` allStartingPositions
+
 positionToIndex :: Position -> Int
-positionToIndex (x,y) = y * labyrinthSize + x
+positionToIndex (x,y) = x * labyrinthSize + y
 
 indexToPosition :: Int -> Position
-indexToPosition index = (index `mod` labyrinthSize, index `div` labyrinthSize)
+indexToPosition index = (index `div` labyrinthSize, index `mod` labyrinthSize)
 
-putTile :: UnfinishedBoard -> (Tile, Position) -> UnfinishedBoard
-putTile []     _           = []
-putTile board  (tile, pos) = replaceAtIndex index (Just tile) board
-  where index = positionToIndex pos
-
-{-|
-  The starting labyrinth, containing only the fixed tiles
--}
-startingLabyrinth :: UnfinishedBoard
-startingLabyrinth = foldl putTile emptyLabyrinth fixedTiles
-
-type XTiles = [XTile]
-freeTiles :: XTiles
-freeTiles = replicate 16 (XTile Corner Nothing)
-  ++ replicate 6 (XTile TShape Nothing)
-  ++ replicate 12 (XTile Line Nothing)
+type FreeTiles = [FreeTile]
+poolOfFreeTiles :: FreeTiles
+poolOfFreeTiles = replicate 16 (FreeTile Corner Nothing)
+  ++ replicate 6 (FreeTile TShape Nothing)
+  ++ replicate 12 (FreeTile Line Nothing)
 
 type FreePositions = [Position]
-freePositions :: FreePositions
-freePositions = map indexToPosition
+poolOfFreePositions :: FreePositions
+poolOfFreePositions = map indexToPosition
   $ filter (`notElem` fixedIndex) [0..(labyrinthSize * labyrinthSize - 1)]
   where fixedIndex = map (positionToIndex . snd) fixedTiles
 
-{-|
-   Taking a free tile by index returns that free tile PLUS the remaining free tiles
-   To achieve this, this function makes use of the State Monad
--}
-takeFreeTile :: Int -> State XTiles XTile
-takeFreeTile index = state $ \tiles -> (tiles!!index, deleteAtIndex index tiles)
+shuffle :: [a] -> StdGen -> ([a], StdGen)
+shuffle []   generator = ([], generator)
+shuffle list generator = let (index,nextGenerator) = randomR (0,length list -1) generator
+                             (listUntilIndex, element:listAfterIndex) = splitAt index list
+                             (shuffledRest, lastGenerator) = shuffle (listUntilIndex ++ listAfterIndex) nextGenerator
+                         in  (element : shuffledRest, lastGenerator)
 
 {-|
-   Taking a free position by index returns that position PLUS the remaining free positions
+ Takes a list of free tiles and returns a list of tiles with random directions
 -}
-takeFreePosition :: Int -> State [Position] Position
-takeFreePosition index = state $ \ps -> (ps!!index, deleteAtIndex index ps)
+createTiles :: [FreeTile] -> StdGen -> ([Tile], StdGen)
+createTiles []        generator = ([], generator)
+createTiles freeTiles generator = let (randomDirection, nextGenerator) = random generator
+                                      (FreeTile kind treasure:ts) = freeTiles
+                                      (tiles, lastGenerator) = createTiles ts nextGenerator
+                                  in  (Tile kind treasure randomDirection : tiles, lastGenerator)
+
+{-|
+ Takes a list of free tiles with their assigned positions and returns a fully complete board
+-}
+createBoard :: [(Tile,Position)] -> Board
+createBoard tilesWithAssignedPositions = Board
+  $ map fst
+  $ sortBy (comparing snd)
+  $ map (second positionToIndex) (tilesWithAssignedPositions ++ fixedTiles)
+
+positionsThatCanHaveTreasures :: Board -> [Position]
+positionsThatCanHaveTreasures (Board tiles) = filter (not . isStartingPosition)
+  $ map indexToPosition [0..(length tiles - 1)]
+
+
+assignRandomPositionsToTreasures :: Board -> [Treasure] -> StdGen -> [(Treasure, Position)]
+assignRandomPositionsToTreasures (Board tiles) treasures generator = let
+  -- positions that can receive a treasure do not include the starting positions
+  poolOfPositions = positionsThatCanHaveTreasures (Board tiles)
+  -- create a list of equal length that sometimes contains a treasure, and sometimes doesn't
+  poolOfTreasures = treasures ++ replicate (length poolOfPositions - length allTreasures) Nothing
+  (shuffledTreasures, newGenerator) = shuffle poolOfTreasures generator
+  -- [(Treasure, Position)]
+  treasuresWithAssignedPositions = zip shuffledTreasures poolOfPositions
+    ++ zip (replicate (length allStartingPositions) Nothing) allStartingPositions
+  in treasuresWithAssignedPositions
+
+putTreasuresOnBoard :: Board -> [(Treasure, Position)] -> Board
+putTreasuresOnBoard (Board tiles) treasuresWithAssignedPositions = let
+  -- [Treasure]
+  treasures = map fst
+    $ sortBy (comparing snd)
+    $ map (second positionToIndex) treasuresWithAssignedPositions
+  -- Create a new board taking into account the assigned treasures
+  newBoard = Board
+    $ map (\(Tile kind oldTreasure direction, newTreasure) -> Tile kind newTreasure direction)
+    $ zip tiles treasures
+  in newBoard
+
+distributeTreasures :: Board -> [Treasure] -> StdGen -> Board
+distributeTreasures board treasures generator = putTreasuresOnBoard board
+  $ assignRandomPositionsToTreasures board treasures generator
+{-|
+ Creates a completely new board by
+  1. Adding a random direction to the pool of free tiles
+  2. Assigning a random free position to those tiles and creating an empty board
+  3. Assigning treasures randomly to the non-starter tiles
+-}
+createNewBoard :: StdGen -> (Board, FreeTile)
+createNewBoard generator = let
+  (tiles, generator1) = createTiles poolOfFreeTiles generator
+  -- There is 1 extra tile, so after shuffling, take the first tile away as the extra tile
+  (Tile k t d : shuffledTiles, generator2) = shuffle tiles generator1
+  extraTile = FreeTile k t
+  -- Shuffle the pool of free positions and assign it to the shuffled tiles
+  (shuffledPositions, generator3) = shuffle poolOfFreePositions generator2
+  board = createBoard (zip shuffledTiles shuffledPositions)
+  boardWithTreasures = distributeTreasures board (map Just allTreasures) generator3
+  in (boardWithTreasures, extraTile)
--- Labyrinth/Models.hs
+++ Labyrinth/Models.hs
@@ -1,26 +1,31 @@
 module Labyrinth.Models (
+  labyrinthSize,
   Kind(..), Direction(..), Treasure, EmptyTile(..),
-  XTile(..), Tile(..), Color, Control,
-  Position, Cards, Player, UnfinishedBoard, Board
+  FreeTile(..), Tile(..), Color(..), Control,
+  Position, Cards, Player, Board(..)
 ) where
 
 import Labyrinth.Helpers
 import Prelude hiding (Right, Left)
 import Data.Char
+import Data.List.Split
+import System.Random
 
-data Kind = Corner | TShape | Line deriving (Eq)
-data Direction = North | East | South | West deriving (Eq)
+labyrinthSize :: Int
+labyrinthSize = 7
+
+data Kind = Corner | TShape | Line deriving (Show, Eq)
+data Direction = North | East | South | West deriving (Show, Eq, Bounded, Enum)
 type Treasure = Maybe Int
 data EmptyTile = EmptyTile Kind Direction deriving(Eq)
-data XTile = XTile Kind Treasure deriving (Eq)
+data FreeTile = FreeTile Kind Treasure deriving (Show, Eq)
 data Tile = Tile Kind Treasure Direction deriving (Eq)
-data Color = Yellow | Red | Blue | Green deriving (Eq)
+data Color = Yellow | Red | Blue | Green deriving (Eq, Bounded, Enum)
 data Control = Human | AI deriving (Eq)
 type Position = (Int, Int)
 type Cards = [Int]
 data Player = Player Color Control Position Cards deriving (Eq)
-type UnfinishedBoard = [Maybe Tile]
-type Board = [Tile]
+newtype Board = Board [Tile]
 
 instance Show EmptyTile
   where show (EmptyTile Corner North) =  " | |_\n\
@@ -71,10 +76,28 @@ instance Show EmptyTile
                                          \_____\n\
                                          \     "
 
-
+treasureToChar :: Treasure -> Char
+treasureToChar Nothing    = ' '
+treasureToChar (Just idx) = ['a'..'z'] !! idx
 
 instance Show Tile where
-  show (Tile kind Nothing direction) = show (EmptyTile kind direction)
-  show (Tile kind (Just treasure) direction) = replaceAtIndex 8 treasureAsChar asEmptyTile
+  show (Tile kind treasure direction) = replaceAtIndex 8 treasureAsChar asEmptyTile
     where asEmptyTile = show (EmptyTile kind direction)
-          treasureAsChar = intToDigit treasure
+          treasureAsChar = treasureToChar treasure
+
+instance Random Direction where
+    random gen = case randomR (fromEnum (minBound :: Direction), fromEnum (maxBound :: Direction)) gen
+                 of (randomIndex, newGen) -> (toEnum randomIndex, newGen)
+    randomR (lower,upper) g = case randomR (fromEnum lower, fromEnum upper) g
+                      of (randomIndex, newGen) -> (toEnum randomIndex, newGen)
+
+showRow :: [Tile] -> String
+showRow rowOfTiles = unlines
+  $ foldl (zipWith (++)) ["", "", ""]
+  $ map (lines . show) rowOfTiles
+
+instance Show Board where
+  show (Board [])     = ""
+  show (Board tiles)  = unlines
+    $ map showRow
+    $ chunksOf labyrinthSize tiles
